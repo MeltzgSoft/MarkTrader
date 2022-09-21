6083cbe42eafb99f68579e21338f4f3fb7b4c601
Greg Meltzer <gregory.m.meltzer@gmail.com>
2022-09-21 11:22:33 -0400

    get redirection working

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def serve(path: str) -> Response:
 
 if __name__ == "__main__":
     config = load_config()
-    app.run(port=config.server.port)
+    app.run(port=config.server.port, ssl_context="adhoc")
--- client/src/apiClient.ts
+++ client/src/apiClient.ts
@@ -4,13 +4,11 @@ interface AuthUri {
     uri: string;
 }
 
-function getAuthUri(brokerageId: string): Promise<AuthUri> {
-    const uri = new URL('/api/v1/auth/${brokerageId}', window.location.href);
-    return fetch(uri)
-        .then(res => res.json())
-        .then(res => {
-            return res as AuthUri;
-        });
+async function getAuthUri(brokerageId: string): Promise<AuthUri> {
+    const uri = new URL(`/api/v1/auth/${brokerageId}`, window.location.origin);
+    const res = await fetch(uri);
+    const res_1 = await res.json();
+    return res_1 as AuthUri;
 }
 
 export {getAuthUri};
