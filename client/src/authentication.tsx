be4be5962e822b7a057c4a04d212d8fa90e4e44f
meltzg <gregory.m.meltzer@gmail.com>
2022-10-22 11:07:19 -0400

    add status bar to sign in

--- client/src/authentication.tsx
+++ client/src/authentication.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
-import {getAuthUri} from './apiClient';
+import { getAuthUri, getSignInStatus } from './apiClient';
+import './authentication.css'
 
-export default class Authenticator extends React.Component {
+interface AuthenticatorState {
+    isSignedIn: boolean;
+}
+
+function AuthenticatorStatus(props: AuthenticatorState) {
+    if (props.isSignedIn) {
+        return <div className='signin-status signed-in'>Connected</div>;
+    } else {
+        return <div className='signin-status signed-out'>Disconnected</div>;
+    }
+}
+
+export default class Authenticator extends React.Component<Record<string, unknown>, AuthenticatorState> {
+    constructor(props: Record<string, unknown>) {
+        super(props);
+        this.state = {
+            isSignedIn: false
+        };
+        getSignInStatus().then(authStatus => this.setState({["isSignedIn"]: authStatus.signedIn}))
+    }
+
     render(): React.ReactNode {
-        return <button onClick={this.initiateAuth}>Sign In</button>;
+        return <div style={{'display': 'inline-block'}}>
+            <AuthenticatorStatus isSignedIn={this.state.isSignedIn}></AuthenticatorStatus>
+            <div>
+                <button onClick={this.initiateAuth}>Sign In</button>
+                <button>Refresh Status</button>
+                <button>Sign Out</button>
+            </div>
+        </div>;
     }
 
     initiateAuth(): void {
