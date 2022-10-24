d56c20315b09aef9144187c0ddfe01330eb490f3
meltzg <gregory.m.meltzer@gmail.com>
2022-10-24 12:59:01 -0400

    add tests for new routes

--- test/backend/unit/api/test_authentication.py
+++ test/backend/unit/api/test_authentication.py
@@ -1,6 +1,12 @@
-import pytest
+import datetime
 
+import pytest
+from mock.mock import PropertyMock, patch
+
+from common.enums import BrokerageId
+from common.models import AuthTokens
 from config import GlobalConfig
+from services.authentication import AuthenticationService
 from services.brokerage import get_brokerage_service
 
 pytestmark = pytest.mark.usefixtures("global_config")
@@ -20,3 +26,44 @@ def test_get_auth_url_not_found(client):
     response = client.get("/api/v1/auth/other")
     assert response.status_code == 422
     assert "Brokerage ID other is not recognized" in response.json["message"]
+
+
+def test_get_auth_status_signed_in(client):
+    config = GlobalConfig()
+    tokens = AuthTokens(
+        BrokerageId.TD,
+        "access_token",
+        datetime.datetime.now() + datetime.timedelta(minutes=30),
+        "refresh_token",
+        datetime.datetime.now() + datetime.timedelta(days=30),
+    )
+    with patch.object(
+        AuthenticationService, "active_tokens", new_callable=PropertyMock
+    ) as mock_tokens:
+        mock_tokens.return_value = tokens
+
+        response = client.get("/api/v1/auth/")
+        assert response.status_code == 200
+        assert response.json["id"] == tokens.brokerage_id.value
+        assert response.json["name"] == config.brokerage_map[tokens.brokerage_id].name
+        assert response.json["signedIn"]
+
+
+def test_get_auth_status_signed_out(client):
+    with patch.object(
+        AuthenticationService, "active_tokens", new_callable=PropertyMock
+    ) as mock_tokens:
+        mock_tokens.return_value = None
+
+        response = client.get("/api/v1/auth/")
+        assert response.status_code == 200
+        assert not response.json["id"]
+        assert not response.json["name"]
+        assert not response.json["signedIn"]
+
+
+def test_sign_out(client):
+    with patch.object(AuthenticationService, "sign_out") as mock_sign_out:
+        response = client.delete("/api/v1/auth/")
+        assert response.status_code == 200
+        mock_sign_out.assert_called_once()
